71d2d01b43a400d6c2933d5d6717e329bc35207e
ghostwolf <ghostwolf@ghostwolf>
2018-09-11 09:52:54 +0200

    improved createdatabase.py

--- www/python/createDatabase.py
+++ www/python/createDatabase.py
@@ -9,8 +9,8 @@ keys = {
         'byFerry': True,
         'klas': 'ICTM1g',
         'bySubway': True,
-        'byTram': True
-        'snoozeBuffer': 15
+        'byTram': True,
+        'snoozeBuffer': 15,
         'OV': True,
         'voornaam': '',
         'alarmVolume': 100,
@@ -29,30 +29,6 @@ Database_Settings = {
     'database': '%s' % databaseName,
     'raise_on_warnings': True
 }
-
-parse_arguments()
-
-#for database connection
-try:
-    #tries to connect to your mariaDB with the given settings, The ** acts as a short cut to pass the right arguments from the
-    #Database_Settings dictionary we setup earlier
-    mariadb_connection = mariadb.connect(**Database_Settings)
-# if the try statements gets an error this part is executed
-except mariadb.Error as err:
-    #checks if the error is an access denied error
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    #checks if the error is a Database does not exist error
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    #if an other error occured it will just dump the entire error
-    else:
-        print("Error: {}".format(err))
-    #stops the program
-    sys.exit(2)
-
-#creates the cursor
-cursor = mariadb_connection.cursor()
 
 def parse_arguments():
     try:
@@ -78,12 +54,36 @@ def parse_arguments():
             print("-t <TABLE> to set the table thats being created")
             sys.exit(2)
 
+parse_arguments()
+
+#for database connection
+try:
+    #tries to connect to your mariaDB with the given settings, The ** acts as a short cut to pass the right arguments from the
+    #Database_Settings dictionary we setup earlier
+    mariadb_connection = mariadb.connect(**Database_Settings)
+# if the try statements gets an error this part is executed
+except mariadb.Error as err:
+    #checks if the error is an access denied error
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    #checks if the error is a Database does not exist error
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    #if an other error occured it will just dump the entire error
+    else:
+        print("Error: {}".format(err))
+    #stops the program
+    sys.exit(2)
+
+#creates the cursor
+cursor = mariadb_connection.cursor()
+
 #creates a settings table in the database
 sql = "CREATE TABLE %s" % settingsTableName
 cursor.execute(sql)
 mariadb_connection.commit()
 
 for k,v in keys.items():
-    sql = "INSERT INTO settings (setting, value) VALUES ('" + str(k) + "','" + str(v) + "')" % k
+    sql = "INSERT INTO settings (setting, value) VALUES ('%s','%s')" % (str(k),str(v))
     cursor.execute(sql)
     mariadb_connection.commit()
