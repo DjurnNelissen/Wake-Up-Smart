9d0030a96368ac1e18b0c4516e10b26b35d4d2d8
ghostwolf <ghostwolf@ghostwolf>
2018-09-11 23:22:01 +0200

    created a getSettings for the back-end so they can be loaded into the form on the front-end

--- www/api/getSettings.py
+++ www/api/getSettings.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
+#add the folder where the common code is stored so we can import it
+import sys
+sys.path.insert(0, '../python')
+
 #import common code and the json lib
 import common, json
+
 #create new common object so we can access common code
 c =  common.common()
 #get settings from the database
