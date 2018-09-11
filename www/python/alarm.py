1ce3820df66f59191095679fc92d0ea16c1a2e1e
DjurnNelissen <djurn98@live.nl>
2018-09-11 11:59:44 +0200

    Replaced playsound module with pygame.

--- www/python/alarm.py
+++ www/python/alarm.py
@@ -1,13 +1,15 @@
 from sense_hat import SenseHat
-from playsound import playsound
+import pygame
 import datetime
 import time
 
 sense = SenseHat()
 sense.rotation = 180
 
+pygame.init()
 
 x = datetime.datetime.now()
 
-playsound('../resources/alarm.mp3')
+pygame.mixer.music.load('alarm.mp3')
+pygame.mixer.music.play(1)
 print("Current time:", x.strftime("%H"), ":" ,x.strftime("%M"))
