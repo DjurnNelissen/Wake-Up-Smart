bf96d188c0b354818073f7e15a08375e9a32a559
DjurnNelissen <djurn98@live.nl>
2018-09-12 11:41:11 +0200

    Updated to Jort's latest version on 12/09 11:40

--- www/python/common.py
+++ www/python/common.py
@@ -1,6 +1,7 @@
-import datetime, json
+import datetime, json, time
 import mysql.connector as mariadb
-import urllib.request
+from mysql.connector import errorcode
+import urllib2
 
 class common:
 
@@ -8,6 +9,9 @@ class common:
     OV_API = 'https://api.9292.nl/0.1'
     Google_API = "http://maps.googleapis.com/maps/api/distancematrix/json?"
     Windesheim_API = "http://roosterplusprod.cloudapp.net/api/"
+
+    Windesheim_API_cooldown = 10 #ensures x seconds between each API call, from this instance
+    Last_Windesheim_API_call = 0
 
     #variables
     mariadb_connection = None
@@ -18,12 +22,16 @@ class common:
         'user': 'ghostwolf',
         'password': '',
         'host': 'localhost',
-        'database': 'SmartWake'
+        'database': 'SenseHatData',
         'raise_on_warnings': True
     }
 
     #location variables
     Windesheim_OV_location = "zwolle_hogeschool-windesheim-loc-campus"
+
+    #creates database connection upon creation
+    def __init__(self):
+        self.connect_to_database()
 
     #updates the settings used to connect to the database
     def set_database_settings(self, settings):
@@ -33,31 +41,38 @@ class common:
     #returns the entire settings table from the database
     def get_settings(self):
         #checks if there is an database connection
-        if not self.mariadb_connection or not self.cursor:
-            #if not  it creates a database connection
-            self.connect_to_database()
+        self.check_database_connection()
         #creates an sql string
-        sql = "SELECT * FROM settings"
+        sql = "SELECT setting, value FROM settings"
         #executes the sql string
         err = self.execute_sql(sql)
         if err:
             return err
         #fetches the result from the sql
         val = self.cursor.fetchall()
-        return val
+        encoded = [[s.encode('utf8') for s in t] for t in val]
+        result = {}
+
+        for k,v in encoded:
+            result[k] = v
+
+        return result
+
+    def check_database_connection(self):
+        #checks of there is an database connection
+        if self.mariadb_connection == None or self.cursor == None:
+            self.connect_to_database()
 
     #gets the value of a setting from the database
     def get_setting_value(self, setting):
-        #checks of there is an database connection
-        if not self.mariadb_connection or not self.cursor:
-            self.connect_to_database()
+        self.check_database_connection()
 
-        sql = "SELECT value FROM settings WHERE setting=%s" % setting
+        sql = "SELECT value FROM settings WHERE setting='%s'" % setting
+
         err = self.execute_sql(sql)
         if err:
             return err
         val = self.cursor.fetchone()
-
         return val
 
     #connects the common module to the database
@@ -70,52 +85,64 @@ class common:
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 return "Database does not exist"
             else:
-                return ("Error: {}".format(err)
-
+                return ("Error: {}".format(err))
+        self.mariadb_connection.text_factory = str
         self.cursor = self.mariadb_connection.cursor()
+
 
     #gets the current les rooster for a klas
     def get_rooster(self, klas):
-        return self.get_data(Windesheim_API + "Klas/%s/les" % klas)
+        t = time.time()
+        #ensures that x seconds have passed since last api call
+        if not t >= self.Last_Windesheim_API_call + self.Windesheim_API_cooldown:
+            #if not wait till x secods have actually passed
+            time.sleep(self.Last_Windesheim_API_call + self.Windesheim_API_cooldown - t)
+        self.Last_Windesheim_API_call = time.time()
+        return self.get_data(self.Windesheim_API + "Klas/%s/les" % klas)
 
     #gets json from a page, used for interacting with API's
-    def get_data(self, link, data):
+    def get_data(self, link, **data):
         if data:
-            for k,v in data.items():
+            #adds each data item to the end of the link as a query
+            for k,v in data['data'].items():
                 link += k + '=' + str(v) + "&"
-
+            #removes the last '&' thats left over
             link = link[:-1]
 
-        contents = urllib.request.urlopen(link).read()
+        contents = urllib2.urlopen(link).read()
+        #returns the result in JSON format
         js = json.loads(contents.decode('utf-8'))
 
         return js
-
-
-    # -------------------- PSEUDO CODE ---------------------------#
 
     #gets the first lesson of a day
     def get_schoolStartTime(self, date):
         rooster = self.get_rooster(self.get_setting_value('klas'))
         todays_classes = []
+        date = date.strftime("%Y-%m-%dT00:00:00Z")
         for l in rooster:
             #loop over all classes in semester
-            if l.date == date:
+            if l['roosterdatum'] == date:
                 #match all classes on the  day
                 todays_classes.append(l)
 
         #order classes by date
-        todays_classes = sorted(todays_classes, key=lambda lesson: lesson.date)
+        todays_classes = sorted(todays_classes, key=lambda lesson: lesson['starttijd'])
         #return earliest date
         if todays_classes[0]:
-            return todays_classes[0].date
+            return todays_classes[0]['starttijd']
         #returns none if there are no classes on the day
         return None
 
     #get the time you have to leave for travel with OV
     def get_OV_departureTime(self):
-        link = OV_API + "/journeys?"
+        link = self.OV_API + "/journeys?"
         sett = self.get_settings()
+        t = self.get_schoolStartTime(datetime.datetime.now())
+        t = str(t)
+        t = t[:-3]
+        t = datetime.datetime.fromtimestamp(float(t)).strftime("%Y-%m-%dT%H%M")
+
         TravelSettings = {
             'before': 1,
             'sequence': 1,
@@ -126,15 +153,15 @@ class common:
             'byTrain': sett['byTrain'],
             'lang': 'nl-NL',
             'from': sett['woonplaats'],
-            'dateTime': self.get_schoolStartTime(datetime.datetime.now()), #get school start time for today
+            'dateTime': t, #get school start time for today
             'searchType': 'arrival',
             'interchangeTime': 'standard',
             'after': 5,
-            'to': Windesheim_OV_location,
+            'to': self.Windesheim_OV_location,
             'realtime': True
         }
 
-        journey = self.get_data(link,TravelSettings)
+        journey = self.get_data(link,data=TravelSettings)
         return journey['journeys'][0]['departure']
 
     #executes an SQL string, returns error if one occured else returns none
@@ -147,9 +174,12 @@ class common:
 
     #calculates the time the alarm has to trigger
     def get_alarmTime(self):
+        Snooze = self.get_setting_value('snoozeBuffer')
         OV_enabled = self.get_setting_value('OV')
         if OV_enabled:
-            return self.get_OV_departureTime() - self.get_setting_value('snoozeBuffer')
+            t = self.get_OV_departureTime() #- datetime.timedelta(minutes=int(Snooze))
+            self.update_setting('alarmTijd',t) # adds the alarm time to the database
+            return t
 
     #updates a setting in the database, creates a new field if it doesnt exist
     def update_setting(setting, value):
