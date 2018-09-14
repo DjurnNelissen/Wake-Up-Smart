56afcab5228c98bea3ce64ad44955a4341645acb
ghostwolf <ghostwolf@ghostwolf>
2018-09-14 11:37:20 +0200

    ...

--- api/testAlarm.py
+++ api/testAlarm.py
@@ -1,11 +1,34 @@
-import common, pygame
+#!/usr/bin/python
 
-pygame.init()
+import sys
+sys.path.insert(0, '../python')
 
-pygame.mixer.music.load(c.get_setting_value('alarmFile'))
-pygame.mixer.music.set_volume(c.get_setting_value('alarmVolume'))
+import common, json, time, wave, pyaudio
 
-pygame.mixer.music.play(1)
+c = common.common()
+chunk = 1024
+
+p = pyaudio.PyAudio()
+
+f = c.get_setting_value('alarmFile')
+m = wave.open(f)
+
+s = p.open(
+    format = p.get_format_from_width(m.getsampwidth()),
+    channels = m.getnchannels(),
+    rate = m.getframerate(),
+    output=True
+)
+
+data = m.readframes(chunk)
+
+while data:
+    s.write(data)
+    data = m.readframes(chunk)
+
+s.stop_stream()
+s.close()
+p.terminate()
 
 print("Content-type: application/json\n")
 print(json.dumps({'succes': True}, default=str))
